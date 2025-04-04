91a4b8b03bd34ae9fcc4451265b3a4693eb314f3
vgsanjaykumar <143058056+vgsanjaykumar@users.noreply.github.com>
2025-04-04 17:00:03 +0530

    k

--- Front-end/Studivo/src/App.jsx
+++ Front-end/Studivo/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className="text-center ">Welcome to Studivo</h1>
+      <h1 className="text-center m-10 text-red-400  ">Welcome to Studivo</h1>
     </div>
   )
 }
--- Front-end/Studivo/tailwind.config.js
+++ Front-end/Studivo/tailwind.config.js
@@ -5,7 +5,31 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        bodyFont: ["Poppins", "sans-serif"],
+        titleFont: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        bodyColor: "#212428",
+        lightText: "#c4cfde",
+        boxBg: "linear-gradient(145deg, #1e2024, #23272b)",
+        designColor: "orange"
+      },
+      boxShadow: {
+        shadowOne: "10px 10px 19px #1c1e22, -10px -10px 19px #262a2e",
+      },
+      screens: {
+        xs: "320px",
+        sm: "375px",
+        sml: "500px",
+        md: "667px",
+        mdl: "768px",
+        lg: "960px",
+        lgl: "1024px",
+        xl: "1280px",
+      }
+    },
   },
   plugins: [],
 }
